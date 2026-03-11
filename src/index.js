5b5dc7d7c9ef6d361b22c87a766a23e53ad0d861
sahuarjun638 <arjunkumarsahu2019@gmail.com>
2026-03-11 17:36:57 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ import connectDB from "./db/index.js";
 dotenv.config()
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at: ${process.env.PORT} `);
+    })
+})
+.catch( (err) => {
+    console.log("Monogdb connection failed!!", err)
+})
 
 
 
---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+const { m } = require("framer-motion")
+
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message="Something went wrong",
+        error = [],
+        stack = ""
+    ){
+        super(message),
+        this.statusCode = statusCode,
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+        if (stack) {
+            this.stack = stack
+        }  else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    } //overwrite karne ke liye 
+} 
+
+export {ApiError}
